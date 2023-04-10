890e7fd2fdfff4e4ba3d0bc29961390b75f88af3
Scott Jones <scott@stackless.dev>
2023-04-10 09:00:23 -0400

    fixed navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <nav className="navbar navbar-dark bg-dark navbar-expand">
             <Link to="/" className="navbar-brand">Exercise Tracker</Link>
             <div className="collapse navbar-collapse">
                 <ul className="navbar-nav mr-auto">
